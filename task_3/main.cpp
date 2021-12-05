d5873118068e93d0e9ccc7ffb63bc055f68458db
k.roshchina <k.roshchina@corp.mail.ru>
2021-12-05 17:40:18 +0300

    task_3: notparallel version

--- task_3/main.cpp
+++ task_3/main.cpp
@@ -1,228 +1,165 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <cmath>
-#include "rand.h"
-
-#include <mpi.h>
 
 #define PI 3.141592653589793238463
-#define S 100 // chunk size
-#define SOLUTION 4 * PI / 3
-#define P 2*2*2
 
-double* get_empty_chunk(){
-    double* result = (double*) malloc(S * 3 * sizeof(double));
-    return result;
+double N, K;
+double Lx, Ly, Lz;
+double hx, hy, hz;
+double tau;
+double at;
+
+double xi(int i) { 
+    return hx * i;
 }
 
-class ChunkBuf {
-    private:
-
-    double** buf;
-    int head, size;
-    ChunkRand* cr;
-
-    public:
-
-    ChunkBuf(int size, ChunkRand* cr) {
-        this->size = size;
-        this->cr = cr;
-        head = -1;
-        buf = (double**) malloc(size * sizeof(double*));
-        for (int i = 0; i < size; ++i)
-            buf[i] = get_empty_chunk();
-    }
-
-    int get_head(){
-        return head;
-    }
-
-    bool empty(){
-        return head < 0;
-    }
-
-    bool full(){
-        return head == size - 1;
-    }
-
-    void gen(){
-        if (full())
-            return;
-        cr->get_random_chunk(buf[++head], S);
-    }
-
-    double* pop(){
-        if (empty())
-            return NULL;
-        return buf[head--];
-    }
-
-    ~ChunkBuf(){
-        for (int i = 0; i < size; ++i)
-            free(buf[i]);
-        free(buf);
-    }
-
-};
-
-void copy_chunk(double* c1, double* c2){
-    for (int i = 0; i < S * 3; ++i)
-        c2[i] = c1[i];
+double yj(int j) { 
+    return hy * j;
 }
 
-// n - number of slaves
-void master(double eps, int n, ChunkRand* cr, std::ostream& error_file){
-    double startTime = MPI_Wtime();
-
-    int flag;
-    MPI_Status status;
-    double sum = 0, p = 0; // number of points computed
-    double** chunks = (double**) malloc(n * sizeof(double*));
-    double* sums = (double*) malloc(n * sizeof(double));
-    MPI_Request* sends = (MPI_Request*) malloc(n * sizeof(MPI_Request));
-    MPI_Request* recvs = (MPI_Request*) malloc(n * sizeof(MPI_Request));
-    ChunkBuf buf(n, cr);
-
-    for (int i = 0; i < n; ++i) {
-        chunks[i] = get_empty_chunk();
-    }
-
-    for (int i = 0; i < n; ++i){
-        cr->get_random_chunk(chunks[i], S);
-
-        MPI_Isend(chunks[i], S * 3, MPI_DOUBLE, i + 1, 0, MPI_COMM_WORLD, &sends[i]);
-        MPI_Irecv(&sums[i], 1, MPI_DOUBLE, i + 1, 1, MPI_COMM_WORLD, &recvs[i]);
-    }
-
-    while (true) {
-        int index;
-        MPI_Testany(n, recvs, &index, &flag, &status);
-        if (flag) {
-            sum += sums[index];
-            p += S; // chunk size
-            // error_file << "full: sum=" << sum << ", p=" << p << ", sol=" << P * sum / p << ", var=" << std::abs(SOLUTION - P * sum / p) << std::endl;
-            // std::flush(error_file);
-            if (std::abs(SOLUTION - P * sum / p) <= eps)
-                break;
-
-            MPI_Test(&sends[index], &flag, &status);
-            if (flag) {
-                // error_file << "send> " << index << std::endl;
-
-                if (!buf.empty()){
-                    // error_file << "pop buf" << std::endl;
-                    copy_chunk(buf.pop(), chunks[index]);
-                } else {
-                    // error_file << "gen by myself" << std::endl;
-                    cr->get_random_chunk(chunks[index], S);
-                }
-
-                MPI_Isend(chunks[index], S * 3, MPI_DOUBLE, index + 1, 0, MPI_COMM_WORLD, &sends[index]);
-            }
-
-            MPI_Irecv(&sums[index], 1, MPI_DOUBLE, index + 1, 1, MPI_COMM_WORLD, &recvs[index]);
-
-            continue;
-        }
-
-        MPI_Testany(n, sends, &index, &flag, &status);
-        if (flag) {
-            // error_file << "send more> " << index << std::endl;
-            if (!buf.empty()){
-                // error_file << "pop buf" << std::endl;
-                copy_chunk(buf.pop(), chunks[index]);
-            } else {
-                // error_file << "gen by myself" << std::endl;
-                cr->get_random_chunk(chunks[index], S);
-            }
-
-            MPI_Isend(chunks[index], S * 3, MPI_DOUBLE, index + 1, 0, MPI_COMM_WORLD, &sends[index]);
-            continue;
-        }
-
-        // error_file << "fill buf: " << buf.get_head()+1 << std::endl;
-        buf.gen();
-        // std::flush(error_file);
-    }
-
-    for (int i = 0; i < n; ++i) {
-        free(chunks[i]);
-    }
-
-    free(chunks);
-    free(sums);
-    free(sends);
-    free(recvs);
-
-    error_file << "RESULT\nSolution=" << P * sum / p << ", Delta=" << std::abs(SOLUTION - P * sum / p) << ", Points=" << p << std::endl;
-    error_file << "Time=" << MPI_Wtime() - startTime << std::endl;
-    std::flush(error_file);
-
-    MPI_Abort(MPI_COMM_WORLD, 0);
+double zk(int k) { 
+    return hz * k;
 }
 
-// void slave(int rank, std::ofstream& error_file){
-void slave(int rank){
-    bool first_iteration = true;
-    int flag;
-    MPI_Status status;
-    double *chunk = get_empty_chunk(), *chunk_recv = get_empty_chunk(), *tmp;
-    double *sum_p = (double*) malloc(2 * sizeof(double));
-    double sum, sum_send;
-    MPI_Request send, recv;
+double vget(std::vector<double> v, int i, int j, int k) {
+    if (i * (N+1) * (N+1) + j * (N+1) + k < (N+1) * (N+1) * (N+1))
+        return v[i * (N+1) * (N+1) + j * (N+1) + k];
 
-    while (true) {
-        if (first_iteration)
-            MPI_Irecv(chunk_recv, S * 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &recv);
+    std::cout << "oh shit we'r out of range in GET: " << i << " " << j << " " << k << std::endl;
+    return -1;
+}
 
-        MPI_Wait(&recv, &status);
-
-        tmp = chunk;
-        chunk = chunk_recv;
-        chunk_recv = tmp;
-
-        MPI_Irecv(chunk_recv, S * 3, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &recv);
-
-        //compute
-        // for (int j = 0; j < 500; ++j) {
-        sum = 0;
-        for (int i = 0; i < S; ++i) {
-            if (chunk[3*i+1] * chunk[3*i+1] + chunk[3*i+2] * chunk[3*i+2] <= 1) {
-                sum += sqrt(chunk[3*i+1] * chunk[3*i+1] + chunk[3*i+2] * chunk[3*i+2]);
-            }
-        }
-        // }
-
-        if (!first_iteration) {
-            MPI_Wait(&send, &status);
-            if (status.MPI_ERROR != MPI_SUCCESS && status.MPI_ERROR != MPI_ERR_LASTCODE) {
-            } else {
-            }
-        }
-
-        sum_send = sum;
-
-        MPI_Isend(&sum_send, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &send);
-
-        if (first_iteration)
-            first_iteration = false;
+void vset(std::vector<double> &v, int i, int j, int k, double val) {
+    if (i * (N+1) * (N+1) + j * (N+1) + k < (N+1) * (N+1) * (N+1)) {
+        v[i * (N+1) * (N+1) + j * (N+1) + k] = val;
+        return;
     }
 
-    free(chunk);
+    std::cout << "oh shit we'r out of range in SET\n"; 
+}
+
+double u(double x, double y, double z, double t) {
+    return sin(PI/Lx * x)*sin(PI/Ly * y)*sin(PI/Lz * z)*cos(at * t);
+}
+
+double phi(double x, double y, double z) {
+    return u(x, y, z, 0);
+}
+
+double delta_h(std::vector<double> u, int i, int j, int k) {
+    double dx = (vget(u, i - 1, j, k) - 2 * vget(u, i, j, k) + vget(u, i + 1, j, k)) / (hx * hx); 
+    double dy = (vget(u, i, j - 1, k) - 2 * vget(u, i, j, k) + vget(u, i, j + 1, k)) / (hy * hy); 
+    double dz = (vget(u, i, j, k - 1) - 2 * vget(u, i, j, k) + vget(u, i, j, k + 1)) / (hz * hz); 
+
+    return dx + dy + dz;
 }
 
 int main(int argc, char **argv){
-    // sqrt(y * y + z * z)
-    // 0 <= x <= 2, y * y + z * z <= 1
 
-    if (argc != 2) {
-        std::cout << "Invalid number of parameters " << argc << ", must be 2 (programm, eps)\n";
+    if (argc != 6) {
+        std::cout << "Invalid number of parameters " << argc << ", must be 6 (program, Lx, Ly, Lz, N, K)\n";
     }
     
-    double eps;
-    eps = strtod(argv[1], NULL);
+    Lx = strtod(argv[1], NULL);
+    Ly = strtod(argv[2], NULL);
+    Lz = strtod(argv[3], NULL);
 
+    N = strtod(argv[4], NULL);
+    K = strtod(argv[5], NULL);
+
+    hx = Lx / N;
+    hy = Ly / N;
+    hz = Lz / N;
+
+    tau = 1 / K;
+
+    at = PI * sqrt(1/(Lx*Lx) + 1/(Ly*Ly) + 1/(Lz*Lz));
+
+    printf("hx=%lf hy=%lf hz=%lf tau=%lf at=%lf\nK=%lf\n", hx, hy, hz, tau, at, K);
+
+
+// u0
+    std::vector<double> u0((N+1)*(N+1)*(N+1)), u1((N+1)*(N+1)*(N+1));
+    for (int i = 0; i <= N; ++i) {
+        for (int j = 0; j <= N; ++j) {
+            for (int k = 0; k <= N; ++k) {
+                if (i == 0 || j == 0 || k == 0 || i == N || j == N || k == N) {
+                    vset(u0, i, j, k, 0);
+                    continue;
+                }
+
+                double x_i = xi(i), y_j = yj(j), z_k = zk(k);
+                
+                double u0_ijk = phi(x_i, y_j, z_k);
+                vset(u0, i, j, k, u0_ijk);
+            }
+        }
+    }
+
+// u1
+    for (int i = 0; i <= N; ++i) {
+        for (int j = 0; j <= N; ++j) {
+            for (int k = 0; k <= N; ++k) {
+                if (i == 0 || j == 0 || k == 0 || i == N || j == N || k == N) {
+                    vset(u1, i, j, k, 0);
+                    continue;
+                }
+
+                vset(u1, i, j, k, vget(u0, i, j, k) + tau * tau / 2 * delta_h(u0, i, j, k));
+            }
+        }
+    }
+
+// un
+    std::vector<double> un_plus_1((N+1)*(N+1)*(N+1)), un_sub_1 = u0, un = u1;
+    double diff;
+    for (int n = 0; n < 18; ++n) { // 18 more steps
+        diff = 0;
+        for (int i = 0; i <= N; ++i) {
+            for (int j = 0; j <= N; ++j) {
+                for (int k = 0; k <= N; ++k) {
+                    if (i == 0 || j == 0 || k == 0 || i == N || j == N || k == N) {
+                        vset(un_plus_1, i, j, k, 0);
+                        continue;
+                    }
+
+                    double un_plus_1_ijk = tau * tau * delta_h(un, i, j, k) + 2 * vget(un, i, j, k) - vget(un_sub_1, i, j, k);
+
+                    vset(un_plus_1, i, j, k, un_plus_1_ijk);
+
+                    diff = fmax(diff, fabs(un_plus_1_ijk - u(xi(i), yj(j), zk(k), tau * (n + 2))));
+                    // if (fabs(un_plus_1_ijk - u(xi(i), yj(j), zk(k), tau * (n + 2))) > 0)
+                        // printf("(%d, %d, %d) = %f - %f\n", i, j, k, un_plus_1_ijk, u(xi(i), yj(j), zk(k), tau * (n + 2)));
+                }
+            }
+        }
+        un_sub_1 = un;
+        un = un_plus_1;
+    }
+
+// output
+
+    std::cout << "diff=" << diff << std::endl;
+
+    // printf("\n\n");
+
+    // for (int i = 0; i <= N; ++i) {
+    //     for (int j = 0; j <= N; ++j) {
+    //         printf("u1[%d][%d] = [", i, j);
+    //         for (int k = 0; k <= N; ++k) {
+    //             if (k != N)
+    //                 printf("%.2f, ", vget(un, i, j, k));
+    //             else
+    //                 printf("%.2f", vget(un, i, j, k));
+    //         }
+    //         printf("]\n");
+    //     }
+    // }
+
+    /*
     int size, rank;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -234,15 +171,9 @@ int main(int argc, char **argv){
         MPI_Finalize();
         return 0;
     }
+    */
 
-    ChunkRand cr(0, 2, -1, 1, -1, 1);
-
-    if (!rank)
-        master(eps, size - 1, &cr, std::cout);
-    else
-        slave(rank);
-
-    MPI_Finalize();
+    // MPI_Finalize();
 
     return 0;
 }
